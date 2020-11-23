2ea01fb903652abb6f2a39473ef1072e2432ccba
maciej.glowacki <maciej.glowacki@appliedtechnology.se>
2020-11-23 17:03:39 +0100

    Boilerplate

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const proAuthRoutes = require("./routes/pro");
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth", proAuthRoutes);
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.55hby.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
