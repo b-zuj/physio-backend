9c335141ff972ce358ebe935d1486c94f940afcd
barbara zujewska <barbara@Salt-Admins-MacBook-Pro.local>
2020-11-26 14:20:20 +0100

    populate clients when requesting pro

--- controllers/pro.js
+++ controllers/pro.js
@@ -1,7 +1,7 @@
 const Pro = require('../models/Pro');
 
 module.exports = {
-  getProData: async id => Pro.findOne({ _id: id }),
+  getProData: async id => Pro.findOne({ _id: id }).populate('clients').exec(),
   updatePro: async (id, data) => Pro.findByIdAndUpdate(id, data, {new: true}),
   deletePro: async id => Pro.deleteOne({ _id: id }),
 };
